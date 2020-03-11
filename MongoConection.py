da09474d6ec627e2edd0cae910e4fd6f1d4fa049
CARLOS ADOLFO DUARTE MARTINEZ <carlos.duarte@ibm.com>
2020-03-11 11:15:47 -0600

    Api integration

--- MongoConection.py
+++ MongoConection.py
@@ -1,50 +1,68 @@
 from pymongo import MongoClient
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime as dt
 import requests
+import json
 from sklearn.linear_model import LinearRegression
 
-diseases = requests.get('http://54.211.232.101:8080/api/')
-mongo_uri = 'mongodb://%s:%s@%s:%s/%s' % ('carlos', 'carlos123', 'localhost', '27017', 'testdb')
-client = MongoClient(mongo_uri)
-db = client["testdb"]
-forecast = db['forecast']
+
+diseases = requests.get('http://54.211.232.101:8080/api/diseases/')
+
+
 for i in diseases.json():
     print(i)
 
-    cursor = db.case.aggregate([{"$match":{'disease': i}},{"$group": {"_id": "$date", "cases":{"$sum":1}}}])
+    cursor = requests.get("http://localhost:8080/api/diseases/report/"+i)
+    print(cursor)
+    l = list(cursor.json())
 
-    df = pd.DataFrame(list(cursor))
-    df.plot(x='_id', y='cases')
-    X = df["_id"].map(dt.datetime.toordinal)
+    df = pd.DataFrame(l)
+    print(df.head())
+    df['date'] = pd.to_datetime(df['date'])
+    X = df["date"].map(dt.datetime.toordinal)
 
     x = X.values.reshape(-1, 1)
-
-    plt.title('Disease track')
-    plt.xlabel('Date')
-    plt.ylabel('Cases')
 
     model = LinearRegression()
     model.fit(x, df['cases'])  # train the model
 
-    start = df["_id"].max()-dt.timedelta(days=10)
+    start = df["date"].max() - dt.timedelta(days=10)
     print(start)
-    datelist = pd.date_range(start.strftime("%d-%m-%Y"), end=dt.datetime.today()+dt.timedelta(days=30))
+    datelist = pd.date_range(start.strftime("%d-%m-%Y"), end=dt.datetime.today() + dt.timedelta(days=30))
 
     X_test = datelist.map(dt.datetime.toordinal)
     x_test = X_test.values.reshape(-1, 1)
     y_predict = model.predict(x_test)
-    my_dates = {}
-    for i, val in enumerate(x_test):
-        d = dt.date.fromordinal(val[0]).strftime('%Y-%m-%d')
-        my_dates[d] = y_predict[i]
+    my_dates = []
+    print(type(my_dates))
+    forecast = pd.DataFrame()
+    forecast['cases'] = list(y_predict)
 
-    print(my_dates)
-    forecast.insert_one({"Test": my_dates})
+    for j in x_test:
+        d = dt.date.fromordinal(j[0]).strftime('%Y-%m-%d')
+        my_dates.append(d)
+    forecast['dates'] = my_dates
+    fc = forecast.to_numpy()
 
-print(model.intercept_, model.coef_)
+    dates = []
+    for d in df['date']:
+        print(type(d))
+        temp = d.to_pydatetime()
+        dates.append(temp.strftime('%Y-%m-%d'))
+    df['dates'] = dates
+    del df['date']
+    data = df.to_numpy()
+    lData = data.tolist()
 
-plt.plot(datelist, y_predict, color='red', linewidth=2)
+    body = json.dumps({"disease": i, "date": dt.date.today().strftime('%Y-%m-%d'),
+                       "forecast": fc.tolist(), "data": lData[::-1]})
+    print(body)
+    #req = requests.post("http://localhost:8080/forecast", body)
+   # print(req.json())
+    print(model.intercept_, model.coef_)
 
-#plt.show()
+# plt.plot(datelist, y_predict, color='red', linewidth=2)
+
+# plt.show()
